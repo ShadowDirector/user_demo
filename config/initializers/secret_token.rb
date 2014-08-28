ab90ab3e4d9f0048a0a80f6dc2ad26fc3c8bad8d
Shadow <trident.systems.international@gmail.com>
2014-08-27 20:31:57 -0400

    initial user login created with views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserDemo::Application.config.secret_key_base = 'aa23b410b7cd8455903bb6f7424d2091c7487f42a3955844f91c85fbb3f96bf8be2ed3ae0f816cdc3044c9ab01658d2ef1cfed82ba8f2f0b292b87980892126b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UserDemo::Application.config.session_store :cookie_store, key: '_user_demo_session'
